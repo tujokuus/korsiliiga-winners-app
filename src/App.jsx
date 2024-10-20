6b37cdc7853dbca204b7f0207d82f9c941322058
tujokuus <72389463+tujokuus@users.noreply.github.com>
2024-10-20 13:29:17 +0300

    radio button komponentti lisätty

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
+// Voittajien valitsemis komponentti jossa käytetään radio buttonia
+const SelectWinners = () => {
+  return (
+    <form>
+      Valitse otteluiden voittajat:
+      <div>
+        <input type='radio' id="matchAWinner" name='joukkue' value='loimaa bisons'/>
+        <label for='matchAWinner'> Loimaa Bisons</label>
+        
+        <input type="radio" id="matchAWinner" name="joukkue" value="Kouvot"/>
+        <label for="matchAWinner"> Kouvot</label>
+      </div>
+    </form>
+  )
+}
+
 // ColorChangebutton-komponentti, jossa kaksi nappia
 const ColorChangeButton = ({ color, changeColor, oppositeColor}) => {
   
@@ -10,14 +26,14 @@ const ColorChangeButton = ({ color, changeColor, oppositeColor}) => {
       onClick={changeColor}
       style={{ backgroundColor: color, color: 'white', padding: '10px 20px', border: 'none', cursor: 'pointer' }}
     >
-      Click to change color
+      Loimaa Bisons
     </button>
 
     <button 
       onClick={changeColor}
       style={{ backgroundColor: oppositeColor, color: 'white', padding: '10px 20px', border: 'none', cursor: 'pointer' }}
     >
-      Opposite color
+      Kouvot
     </button>
     </p>
   )
@@ -26,6 +42,15 @@ const ColorChangeButton = ({ color, changeColor, oppositeColor}) => {
 const App = () => {
   const [winnerA, setWinnerA] = useState(0)
   const [color, setColor] = useState('blue')
+  
+  // Esimerkkilistä nimistä
+  const teams = [
+    { id: 1, name: 'Tampereen Pyrintö' },
+    { id: 2, name: 'Bisons' },
+    { id: 3, name: 'Kouvot' },
+    { id: 4, name: 'Karhubasket' },
+    { id: 5, name: 'BC Nokia' }
+  ]
 
   // Funktio vaihtamaan napin väriä
   const changeColor = () => {
@@ -44,10 +69,13 @@ const App = () => {
   return (
     <>
         <h1>Ennusta korisliiga otteluiden voittajat!</h1>
-        <button onClick={() => setWinnerA((winnerA) => winnerA + 1)}>
-          winner is {winnerA}
+        <button 
+        onClick={() => setWinnerA((winnerA) => winnerA + 1)}>
+        winner is {winnerA}
         </button>
+        
         <ColorChangeButton color={color} changeColor={changeColor} oppositeColor={oppositeColor}/>
+        <SelectWinners></SelectWinners>
         
     </>
   )
