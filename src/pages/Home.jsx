ec29453d45e1ba78db2b089a38b239d31411a4fb
tujokuus <72389463+tujokuus@users.noreply.github.com>
2024-11-28 13:41:51 +0200

    update home-page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
+import './Home.css'
+
 const Home = () => {
     return (
         <div className="home-container">
             <h2>Tervetuloa Korisliiga Pick'Em -sivustolle</h2>
             <p>
-                Hei, tervetuloa Korisliiga Pick'Em -sivustolle! Täällä voit veikata korisliigan otteluiden voittajia ja kerätä pisteitä. 
-                Tämä sovellus tarjoaa sinulle mahdollisuuden testata tietämystäsi ja ennustustaitojasi korisliigan otteluissa.
+                Hei, tervetuloa Korisliiga Pick'Em -sivustolle! Tämä sivusto on toteutettu tietotekniikan opiskelijan toimesta harjoitellakseen web-ohjelmointia. 
+                Projekti on rakennettu käyttäen moderneja web-teknologioita, kuten React, Node.js ja Express.js.
+                Sivusto on vielä kesken, eikä joukkueet ja ottelut ole oikeita. 
             </p>
+            <p>
+                Täällä voit veikata korisliigan otteluiden voittajia ja kerätä pisteitä. 
+                Tämä sovellus tarjoaa sinulle mahdollisuuden testata tietämystäsi ja ennustustaitojasi korisliigan otteluissa.
+                </p>
             <p>
                 Voit selata tulevia otteluita, tehdä ennustuksia ja seurata, kuinka hyvin ennustuksesi osuvat oikeaan. 
                 Jokaisen ottelun jälkeen voit tarkistaa, oliko ennustuksesi oikein, ja nähdä, kuinka monta pistettä olet kerännyt.
-            </p>
-            <p>
-                Tämä sivusto on toteutettu tietotekniikan opiskelijan toimesta harjoitellakseen web-ohjelmointia. 
-                Projekti on rakennettu käyttäen moderneja web-teknologioita, kuten React, Node.js ja Express.js.
             </p>
             <p>
                 Toivottavasti nautit sivuston käytöstä ja onnistut tekemään hyviä ennustuksia!
