cac0e4f359bb49e824408b329ee7a180956d9edf
tujokuus <72389463+tujokuus@users.noreply.github.com>
2024-12-02 11:36:46 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -574,7 +574,7 @@ app.patch('/api/predictions/:id', (request, response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`) 
 })
