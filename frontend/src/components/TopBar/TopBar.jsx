84d560c9258a1d2c9042da1b86cd4f5ecbd048e2
tujokuus <72389463+tujokuus@users.noreply.github.com>
2024-12-12 11:23:21 +0200

    updated topBar
    topBar now only visible on pickems page

--- frontend/src/components/TopBar/TopBar.jsx
+++ frontend/src/components/TopBar/TopBar.jsx
@@ -1,7 +1,14 @@
-import './TopBar.css';
-import React from "react";  
+import './TopBar.css'
+import React from "react"  
+import { useLocation } from 'react-router-dom'
 
 const TopBar = ({ handleSend, points, showTopBar }) => {
+const location = useLocation()
+
+if (location.pathname !== '/pickems') {
+    return null
+}
+
     return (
         <div className={`top-bar ${showTopBar ? 'visible' : ''}`}>
             <span>Omat pisteet: {points}</span>
