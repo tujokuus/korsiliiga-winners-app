a0ef58409f728a72e298f01c236e7f4efa68e633
tujokuus <72389463+tujokuus@users.noreply.github.com>
2025-01-13 12:59:15 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,5 @@ import App from './App.jsx'
 import './index.css'
 import axios from 'axios'
 
-axios
-  .get("http://localhost:3001/api/matches")
-  .then(response => {
-    const matches = response.data
-    ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-  })
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
   
